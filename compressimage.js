ac05d743de7f96311063e7eedbaa492048b74f03
Davide Vago <davide.vago@ef.com>
2015-12-02 18:03:36 +0000

    added imagemin and optimage as layer of compression, rebuilt the GM routine 'cause .thumb was resizing without maintain the specified measures

--- compressimage.js
+++ compressimage.js
@@ -2,86 +2,81 @@ var gm = require('gm'),
     fs = require('fs'),
     chalk = require('chalk'),
     execFile = require('child_process').execFile,
+    imagemin = require('imagemin'),
+    optimage = require('optimage'),
     mozjpeg = require('mozjpeg');
 
 module.exports = function(file, width, height, path, filename) {
   
   
   var readStream = fs.createReadStream(file),
-    newFile = path + '/' + filename;
+    newFile = path + '/' + filename,
+    finalCompressor = function(path, newFile, opti) {
+      new imagemin()
+          .src(newFile)
+          .use(imagemin.jpegtran({progressive: true}))
+          .run(function (err, files) {
+
+          optimage({
+            inputFile: newFile,
+            outputFile: newFile
+          }, function (err, res) {
+
+            if (!err) {
+              execFile(mozjpeg, ['-outfile', opti, newFile], function (err) {
+                console.log(chalk.green('new optimised file: ') + opti);
+                setTimeout(function () {
+                  fs.unlink(newFile);
+                }, 100);
+              });
+            }
+          });
+        })
+    };
 
 
-  gm(readStream)
-  .size({bufferStream: true}, function(err, size) {
+  gm(readStream).size({bufferStream: true}, function(err, size) {
 
     switch(true) {
       case(this.data.size.width == width && this.data.size.height == height) :
-        this.quality(90)
-          .minify(5)
-          .write(newFile, function (err) {
-            if (!err) {
-              var ext = newFile.substr(newFile.lastIndexOf('.') + 1),
-                opti = (ext === 'png') ? newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.jpg' : newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.' + ext;
+        this.filter('hamming')
+            .quality(90)
+            .interlace('Line')
+            .strip()
+            .compress('Zip')
+            .write(newFile, function (err) {
+              if (!err) {
+                var ext = newFile.substr(newFile.lastIndexOf('.') + 1),
+                    opti = (ext === 'png') ? newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.jpg' : newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.' + ext;
 
-              execFile(mozjpeg, ['-outfile', opti, newFile ], function (err) {
-                  console.log(chalk.green('New optimised file: ') + opti );
-                  setTimeout(function() {
-                      fs.unlink(newFile);
-                  },100);
-              });
-
-            }
-
-          });
+                finalCompressor(path, newFile, opti);
+              }
+            });
         break;
       case(this.data.size.width > width && this.data.size.height > height) :
           var ext = newFile.substr(newFile.lastIndexOf('.') + 1),
-            opti = (ext === 'png') ? newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.jpg' : newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.' + ext;
+              opti = (ext === 'png') ? newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.jpg' : newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.' + ext;
 
           this.setFormat("jpg")
-          .gravity('Center')
-          .thumb(width, height, newFile, 90, function (err){
-              if (!err) {
-                execFile(mozjpeg, ['-outfile', opti, newFile ], function (err) {
-                    console.log(chalk.green('New optimised file: ') + opti );
-                    setTimeout(function() {
-                        fs.unlink(newFile);
-                    },100);
-                });
-              }
-          });
+              .gravity('Center')
+              .resize(width, height, '^')
+              .crop(width, height)
+              .filter('hamming')
+              .unsharp(0.5)
+              .quality(90)
+              .interlace('Line')
+              .strip()
+              .compress('Zip')
+              .write(newFile, function (err) {
+                if (!err) {
+                  finalCompressor(path, newFile, opti);
+                }
+              });
         break;
       default:
-        console.log(chalk.red('Can\'t optimise this file: ') + newFile + chalk.red(' because the source sizes are: ') + this.data.size.width +'x'+ this.data.size.height + chalk.red(' and the requested output is bigger: ') + width + 'x' + height);
+        console.log(chalk.red('Can\'t optimise this file: ') + newFile + chalk.red(' because the source size is: ') + this.data.size.width +'x'+ this.data.size.height + chalk.red(' and the requested output has a parameter bigger: ') + width + 'x' + height);
         break;
     }
-
-//    if(this.data.size.width > width && this.data.size.height > height) {
-//       this.setFormat("jpg")
-//        .resize(width, height, '^')
-//        .gravity('Center')
-//        .crop(width, height)
-//        .strip()
-//        .quality(90)
-//        .interlace('line')
-//        .compress('JPEG')
-//        .minify(5)
-//        .write(newFile, function (err) {
-//          if (!err) {
-//            var ext = newFile.substr(newFile.lastIndexOf('.') + 1),
-//              opti = (ext === 'png') ? newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.jpg' : newFile.substr(0, newFile.lastIndexOf('.')) + '-optimized.' + ext;
-//
-//            execFile(mozjpeg, ['-outfile', opti, newFile ], function (err) {
-//                console.log(chalk.green('new optimised file: ') + opti );
-//                setTimeout(function() {
-//                    fs.unlink(newFile);
-//                },100);
-//            });
-//
-//          }
-//
-//        });
-//    }
 
   });
   
